694a2e0bfd476f6d4ce622491b3d3dbba33a1b92
lucawsl <luca.paganin1994@gmail.com>
2026-04-11 10:44:51 +0200

    impostato problema eq II grado

---
+++ 2-esercitazione-calcolo_1/eq_secondo_grado.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+    cout << "Inserisci i coefficienti a, b, c: " << endl;
+    cin >> a >> b >> c;
+
+    if (a == 0) {
+        // Equazione di primo grado: bx + c = 0
+        // Scrivi tu qui - attenzione: anche b potrebbe essere 0!
+
+    } else {
+        // Equazione di secondo grado
+        double delta = b*b - 4*a*c;
+
+        // Scrivi tu qui i tre sotto-casi su delta
+
+    }
+
+    return 0;
+}
