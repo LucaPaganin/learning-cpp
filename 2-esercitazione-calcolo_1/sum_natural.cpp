db4d964766e7305284408227f097733c1dc7260e
lucawsl <luca.paganin1994@gmail.com>
2026-04-11 11:12:41 +0200

    sum natural

---
+++ 2-esercitazione-calcolo_1/sum_natural.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 0;
+    cout << "Inserisci n: " << endl;
+    cin >> n;
+
+    int sum = 0;
+    for (int i = 1; i <= n; i++){
+        sum = sum + i;
+        cout << "Passo " << i << " la somma fino a qui e': " << sum << endl;
+        // sum += i;  equivalente a quello sopra
+    }
+    cout << "Somma fino a n = " << n << " e' uguale a " << sum << endl;
+
+    return 0;
+}
